4b63b403640ef853ba9f79f4a57e4bbb41c3cd31
Fadil <rosunmungurudm@gmail.com>
2022-07-17 11:46:24 +0400

    exception subscriber - http code response

--- src/src/EventSubscriber/ExceptionSubscriber.php
+++ src/src/EventSubscriber/ExceptionSubscriber.php
@@ -3,20 +3,34 @@
 namespace App\EventSubscriber;
 
 use App\Services\SecurityToken;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Doctrine\DBAL\Exception\ForeignKeyConstraintViolationException;
+use Symfony\Component\HttpFoundation\Exception\RequestExceptionInterface;
 
 class ExceptionSubscriber implements EventSubscriberInterface
 {
     public function onKernelException(ExceptionEvent $event)
     {
-        $type = $event->getThrowable();
+        $error = $event->getThrowable();
+        $statusCode = null;
+
+        if ($error instanceof HttpExceptionInterface) {
+            $statusCode = $error->getStatusCode();
+        } elseif ($error instanceof RequestExceptionInterface) {
+            $statusCode = Response::HTTP_BAD_REQUEST;
+        }
+
+        if (null === $statusCode) {
+            $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
+        }
 
         $event->setResponse(new JsonResponse([
-            'error' => 'true',
-            'message' => $type->getMessage(),
+            'error' => $statusCode,
+            'error_description' => $error->getMessage(),
         ]));
     }
 
